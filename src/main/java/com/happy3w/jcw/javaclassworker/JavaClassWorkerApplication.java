db12d8e75acd654f52b07dd7b1accfd2445f4f1c
Yushan Gao <ysgao@thoughtworks.com>
2017-08-31 13:56:40 +0800

    Init project

---
+++ src/main/java/com/happy3w/jcw/javaclassworker/JavaClassWorkerApplication.java
@@ -0,0 +1,12 @@
+package com.happy3w.jcw.javaclassworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaClassWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaClassWorkerApplication.class, args);
+	}
+}
