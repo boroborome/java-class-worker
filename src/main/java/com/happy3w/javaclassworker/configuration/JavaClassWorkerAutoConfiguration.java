722787053fbaff83b7e7f9eb7ab5cdc376d7156b
Yushan Gao <ysgao@thoughtworks.com>
2017-09-01 13:50:31 +0800

    Refactor method to load constant

--- src/main/java/com/happy3w/javaclassworker/configuration/JavaClassWorkerAutoConfiguration.java
+++ src/main/java/com/happy3w/javaclassworker/configuration/JavaClassWorkerAutoConfiguration.java
@@ -1,36 +1,27 @@
 package com.happy3w.javaclassworker.configuration;
 
-import com.happy3w.javaclassworker.classfile.constant.*;
+import com.happy3w.javaclassworker.JavaClassWorkerApplication;
+import com.happy3w.javaclassworker.classfile.constant.AbstractConstant;
+import com.happy3w.javaclassworker.classfile.constant.ConstantCenter;
+import com.happy3w.javaclassworker.classfile.constant.ConstantDefine;
+import com.happy3w.javaclassworker.classfile.constant.ConstantTypeInfo;
+import org.reflections.Reflections;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 @Configuration
 public class JavaClassWorkerAutoConfiguration {
 
-    // TOBE Improved with ConstantRegistrar
     @Bean
     public ConstantCenter constantCenter(ApplicationContext applicationContext) {
+        Reflections reflections = new Reflections(JavaClassWorkerApplication.class.getPackage().getName());
         ConstantCenter center = new ConstantCenter();
-        center.init(Arrays.asList(ConstantClass.class,
-                ConstantFieldref.class,
-                ConstantMethodref.class,
-                ConstantInterfaceMethodref.class,
-                ConstantInteger.class,
-                ConstantString.class,
-                ConstantFloat.class,
-                ConstantLong.class,
-                ConstantDouble.class,
-                ConstantNameAndType.class,
-                ConstantUtf8.class,
-                ConstantMethodHandle.class,
-                ConstantMethodType.class,
-                ConstantInvokeDynamic.class)
+        center.init(reflections.getTypesAnnotatedWith(ConstantDefine.class)
                 .stream()
-                .map(t -> ConstantTypeInfo.from(t))
+                .map(t -> ConstantTypeInfo.from((Class<? extends AbstractConstant>) t))
                 .collect(Collectors.toList())
         );
         return center;
